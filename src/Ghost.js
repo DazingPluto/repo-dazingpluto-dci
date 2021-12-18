cf59d80a7a2f348d70cb25430752925df31dee0b
DazingPluto <92892528+DazingPluto@users.noreply.github.com>
2021-12-17 18:55:33 -0600

    Im sorry i tried, ill finish it tonight and re commit

---
+++ src/Ghost.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Ghost from './Ghost';
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <Ghost />
+      </div>
+    )
+  }
+}
+
+export default App;
